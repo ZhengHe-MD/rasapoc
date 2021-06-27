68dae4dd062913d2445f73db0ce82163adf507e6
zhenghe3119 <zhenghe3119@ipalfish.com>
2021-06-27 18:41:15 +0800

    First dialogue

--- shell.sh
+++ shell.sh
@@ -1,5 +1,6 @@
 docker run -it \
+           --rm \
            --user "$(id -u):$(id -g)" \
            -v $(pwd):/app \
            -p 5005:5005 \
-           rasa/rasa:2.7.0-full shell
+           rasa/rasa:2.7.0-full shell --debug
