90cede139a998c0a6ecc748b35ac44fa7f710953
zhenghe3119 <zhenghe3119@ipalfish.com>
2021-06-27 23:55:01 +0800

    Try explain

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,7 @@ import logging
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -30,8 +30,8 @@ class ActionSetFaqSlot(Action):
     ) -> List[Dict[Text, Any]]:
         full_intent = (
             tracker.latest_message.get("response_selector", {})
-            .get("faq", {})
-            .get("full_retrieval_intent")
+                .get("faq", {})
+                .get("full_retrieval_intent")
         )
         logger.info(f"intent: {full_intent}")
 
@@ -40,3 +40,23 @@ class ActionSetFaqSlot(Action):
         else:
             topic = None
         return [SlotSet("faq", topic)]
+
+
+class ActionExplainFaqs(Action):
+    def name(self) -> Text:
+        return "action_explain_faq"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        topic = tracker.get_slot("faq")
+
+        if topic in ["rasax"]:
+            dispatcher.utter_message(template=f"utter_faq_{topic}_more")
+        else:
+            dispatcher.utter_message(template="utter_no_further_info")
+
+        return []
