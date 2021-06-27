e44629b5725ab8c1e8b64a898b6046199d8a435c
zhenghe3119 <zhenghe3119@ipalfish.com>
2021-06-27 21:42:01 +0800

    Add first custom action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,32 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.types import DomainDict
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionSetFaqSlot(Action):
+    def name(self) -> Text:
+        return "action_set_faq_slot"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> List[Dict[Text, Any]]:
+        full_intent = (
+            tracker.latest_message.get("response_selector", {})
+            .get("faq", {})
+            .get("full_retrieval_intent")
+        )
+
+        if full_intent:
+            topic = full_intent.split("/")[1]
+        else:
+            topic = None
+        return [SlotSet("faq", topic)]
